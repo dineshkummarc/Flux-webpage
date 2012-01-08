f9ed50b2b0aba6d63d5c9b0f308ab7548134ee46
David Romaní <david@flux.cat>
2012-01-08 01:00:53 +0100

    Homepage view (I)

---
+++ apps/frontend/modules/home/actions/actions.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * project actions.
+ *
+ * @package    fluxweb
+ * @subpackage project
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class homeActions extends sfActions
+{
+  public function executePage(sfWebRequest $request)
+  {
+    
+  }
+}
