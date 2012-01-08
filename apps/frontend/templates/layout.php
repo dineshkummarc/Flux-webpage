9179899b8922b97bdd0d562a82e5755405961b31
David Romaní <david@flux.cat>
2012-01-08 01:19:24 +0100

    Migration (II)

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -4,11 +4,63 @@
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    <script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
+    <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-26087563-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
   </head>
   <body>
-    <?php echo $sf_content ?>
+  	<div id="header">
+  		<div id="header-left">
+  			<img src="/images/logo.png" alt="logotip empresa Flux"/>
+  		</div>
+  		<div id="header-right">
+  			<ul>
+  				<li><a id="leng">eng</a></li>
+  				<li><a id="lesp">esp</a></li>
+  				<li><a id="lcat">cat</a></li>
+  			</ul>
+  		</div>
+  	</div>
+  	<div id="main">
+  		<div id="main-left">
+  			<ul>
+  				<li id='manifest' class='selected'></li>
+  				<li id='projectes'></li>
+  				<li id='team'></li>
+  				<li id='contact'></li>
+  			</ul>
+  		</div>
+  		<div id="main-right">
+  			<?php echo $sf_content ?>
+  		</div>
+  	</div>
+    <div id="footer">
+			<div id="footer-left">
+				<ul>
+					<li><!-- <g:plusone size="medium"></g:plusone> -->Google+</li>
+					<li><a href="http://twitter.com/share" class="twitter-share-button" data-url="http://www.flux.cat" data-count="horizontal">Tweet</a></li>
+					<li><div id="fb-root" style="padding:0px; margin:0px auto"></div><fb:like href="http://www.flux.cat" send="false" layout="button_count" width="110" show_faces="false" font=""></fb:like></li>
+				</ul>
+			</div>
+			<div id="footer-center">
+				<p><a href="mailto:info@flux.cat">info@flux.cat</a></p>
+			</div>
+			<div id="footer-right">
+				<p><a id="privacitat"></a> · &copy;<?php echo date('Y')?></p>
+			</div>		
+		</div> 
   </body>
 </html>
