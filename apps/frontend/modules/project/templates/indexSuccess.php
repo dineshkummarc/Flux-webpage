8193a2c11936304326c373962ff5b9d89554c038
David Romaní <david@flux.cat>
2012-02-05 19:27:03 +0100

    CSS bug fixed

--- apps/frontend/modules/project/templates/indexSuccess.php
+++ apps/frontend/modules/project/templates/indexSuccess.php
@@ -3,12 +3,14 @@
 <?php if (count($projects) == 0): ?>
 	<p><?php echo __('There are no projects') ?></p>
 <?php else: ?>
+	<?php $i = 0; ?>
 	<?php foreach ($projects as $project): ?>
+		<?php if ($i > 0) echo '<hr/>'?>
 		<?php if (strlen($project->getLogo()) > 0) echo "<img src='".$project->getLogo()."' alt='Logo ".$project->getName()."' />"; ?>
 		<p><?php if ($project->getUnit()->getName() != 'Skaeda') echo $project->getUnit()->getName(); ?></p>
 		<?php echo $project->getUnit()->getSummary(ESC_RAW) ?>
 		<p><?php echo link_to('Read more', 'project/show?id='.$project->getId()); ?></p>
 		<p><?php echo __('Website: ').link_to($project->getUnit()->getUrl(), 'http://'.$project->getUnit()->getUrl()); ?></p>
-		<hr/>
+		<?php $i = 1; ?>
 	<?php endforeach; ?>
 <?php endif; ?>
