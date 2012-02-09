f42c9022d456611fad8ccffabb84e3ef5c27690f
David Romaní <david@flux.cat>
2012-02-09 17:54:16 +0100

    i18n routing system bug fixed

--- apps/frontend/modules/language/actions/actions.class.php
+++ apps/frontend/modules/language/actions/actions.class.php
@@ -4,15 +4,30 @@ class languageActions extends sfActions
 {
 	public function executeChangeLanguage(sfWebRequest $request)
 	{
-		/*
-		 	print ('Request language: '. $request->getGetParameter('lang'). '<br/>');
-			print ('Path info: '. $request->getPathInfo(). '<br/>');
-			print ('Referer: '. $request->getReferer(). '<br/>');
-			print ('Current user culture: '. $this->getUser()->getCulture(). '<br/>');
-		 */
-		$newURL = str_replace('/'.$this->getUser()->getCulture().'/' , '/'.$request->getGetParameter('lang').'/', $request->getReferer());
 		$this->getUser()->setCulture($request->getGetParameter('lang'));
-		return $this->redirect($newURL);
+		$accio = substr(strrchr($request->getReferer(), "/"), 1);
+		
+		if ($accio == 'inicio' || $accio == 'inici' || $accio == 'home') {
+			$newRoute = '@homepage_'.$request->getGetParameter('lang');
+		} else if ($accio == 'proyectos' || $accio == 'projectes' || $accio == 'projects') {
+			$newRoute = '@projectes_'.$request->getGetParameter('lang');
+		} else if ($accio == 'servicios' || $accio == 'serveis' || $accio == 'services') {
+			$newRoute = '@serveis_'.$request->getGetParameter('lang');
+		} else if ($accio == 'equipo' || $accio == 'equip' || $accio == 'team') {
+			$newRoute = '@equip_'.$request->getGetParameter('lang');
+		} else if ($accio == 'contacto' || $accio == 'contacte' || $accio == 'contact') {
+			$newRoute = '@contacte_'.$request->getGetParameter('lang');
+		}
+		
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] Request language: '.$request->getGetParameter('lang'));
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] Path info: '.$request->getPathInfo());
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] Referer: '.$request->getReferer());
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] Current user culture: '.$this->getUser()->getCulture());
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] Action: '.$accio);
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] New route: '.$newRoute);
+		sfContext::getInstance()->getLogger()->debug('[language/changeLanguage] New jump: '.$this->getController()->genUrl($newRoute));
+		
+		return $this->redirect($this->getController()->genUrl($newRoute));
 	}
 
 }
