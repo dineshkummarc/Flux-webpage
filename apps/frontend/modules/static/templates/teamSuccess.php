a0718b171be1b1cfcf474e5fcf88836a0c38c5e6
David Romaní <david@flux.cat>
2012-02-10 14:54:51 +0100

    Frontend team module upgrade I

--- apps/frontend/modules/static/templates/teamSuccess.php
+++ apps/frontend/modules/static/templates/teamSuccess.php
@@ -1,5 +1,4 @@
 <h1><?php echo __('team')?></h1>
-
 <?php if (count($coreMembers) == 0): ?>
 	<p><?php echo __('There are no core members') ?></p>
 <?php else: ?>
@@ -14,3 +13,17 @@
 	<?php endforeach; ?>
 <?php endif; ?>
 
+<h1><?php echo __('partners')?></h1>
+<?php if (count($partnerMembers) == 0): ?>
+	<p><?php echo __('There are no partner members') ?></p>
+<?php else: ?>
+	<?php $i = 0; ?>
+	<?php foreach ($partnerMembers as $member): ?>
+		<?php if ($i > 0) echo '<hr/>'?>
+		<?php if (strlen($member->getAltImage()) > 0) echo "<img src='".$member->getAltImage()."' alt='Photo ".$member->getUser()."' />"; ?>
+		<p><?php echo $member->getName().' '.$member->getSurname() ?><br/>
+		<?php echo $member->getCategory() ?><br/>
+		<?php echo $member->getDescription(ESC_RAW) ?></p>
+		<?php $i = 1; ?>
+	<?php endforeach; ?>
+<?php endif; ?>
