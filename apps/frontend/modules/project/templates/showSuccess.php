bf1fa0fc03902a96ddcec4ef1e8f6509bf59a0fc
David Romaní <david@flux.cat>
2012-02-13 12:20:38 +0100

    Frontend show project update I

--- apps/frontend/modules/project/templates/showSuccess.php
+++ apps/frontend/modules/project/templates/showSuccess.php
@@ -1,9 +1,16 @@
 <h1><?php echo __('projects')?></h1>
 
-<p><?php if (strlen($project->getLogo()) > 0) echo "<img src='".$project->getLogo()."' alt='Logo ".$project->getName()."' />"; ?></p>
-<p><?php if ($project->getUnit()->getName() != 'Skaeda') echo $project->getUnit()->getName(); ?></p>
+<?php if (strlen($project->getLogo()) > 0): ?>
+	<img src='<?php echo $project->getLogo() ?>' alt='Logo <?php echo $project->getName() ?>'/>
+<?php endif; ?>
+
+<?php if ($project->getUnit()->getName() != 'Skaeda'): ?>
+	<p><?php echo $project->getUnit()->getName(); ?></p>
+<?php endif; ?>
+
 <p><?php echo $project->getUnit()->getSubtitle(ESC_RAW) ?></p>
 <p><?php echo $project->getUnit()->getDescription(ESC_RAW) ?></p>
+
 <?php if (strlen($project->getAltImage()) > 0): ?>
 	<img class='projectScreenshot' src='<?php echo $project->getAltImage() ?>' alt='Screenshot <?php echo $project->getName() ?>' />
-<?php endif;?>
+<?php endif; ?>
