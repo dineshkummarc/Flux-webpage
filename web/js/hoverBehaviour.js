703e5970a3bf6e123471f20aa1849b7f35c144dd
David Romaní <david@flux.cat>
2012-02-13 15:38:10 +0100

    aeplaStats project added

--- web/js/hoverBehaviour.js
+++ web/js/hoverBehaviour.js
@@ -2,16 +2,16 @@ function makeHoverInEffect(evt) {
 	var obj = document.getElementById(evt.id);
 	var newSrc = obj.src.substring(0, obj.src.indexOf('.png')) + '-color.png';
 	obj.src = newSrc;
-	console.log('[makeHoverInEffect] Hi! EventID = ' + evt.id);
-	console.log('[makeHoverInEffect] Current SRC = ' + obj.src);
-	console.log('[makeHoverInEffect] New SRC = '     + newSrc);	
+	//console.log('[makeHoverInEffect] Hi! EventID = ' + evt.id);
+	//console.log('[makeHoverInEffect] Current SRC = ' + obj.src);
+	//console.log('[makeHoverInEffect] New SRC = '     + newSrc);	
 }
 
 function makeHoverOutEffect(evt) {
 	var obj = document.getElementById(evt.id);
 	var newSrc = obj.src.substring(0, obj.src.indexOf('-color.png')) + '.png';
 	obj.src = newSrc;
-	console.log('[makeHoverOutEffect] Hi! EventID = ' + evt.id);
-	console.log('[makeHoverOutEffect] Current SRC = ' + obj.src);
-	console.log('[makeHoverOutEffect] New SRC = '     + newSrc);	
+	//console.log('[makeHoverOutEffect] Hi! EventID = ' + evt.id);
+	//console.log('[makeHoverOutEffect] Current SRC = ' + obj.src);
+	//console.log('[makeHoverOutEffect] New SRC = '     + newSrc);	
 }
