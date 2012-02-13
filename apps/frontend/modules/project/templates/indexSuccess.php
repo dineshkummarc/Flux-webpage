d3c21c854faa423ec685da2193cecab582b7ae72
David Romaní <david@flux.cat>
2012-02-13 13:45:46 +0100

    Frontend show project update VI

--- apps/frontend/modules/project/templates/indexSuccess.php
+++ apps/frontend/modules/project/templates/indexSuccess.php
@@ -6,8 +6,8 @@
 	<?php $i = 0; ?>
 	<?php foreach ($projects as $project): ?>
 		<?php if ($i > 0) echo '<hr/>'?>
-		<?php if (strlen($project->getLogo()) > 0) echo link_to(image_tag($project->getLogo(), array('class' => 'logo', 'alt' => 'Logo '.$project->getName())), '@projectes_show_es?id='.$project->getId()); ?>
-		<p><?php if ($project->getUnit()->getName() != 'Skaeda') echo $project->getUnit()->getName(); ?></p>
+		<?php if (strlen($project->getLogo()) > 0) echo link_to(image_tag($project->getLogo(), array('class' => 'logo', 'alt' => 'Logo '.$project->getName())), '@projectes_show_'.$sf_user->getCulture().'?id='.$project->getId()); ?>
+		<!-- <p><?php /*if ($project->getUnit()->getName() != 'Skaeda') echo $project->getUnit()->getName();*/ ?></p> -->
 		<p><?php echo $project->getUnit()->getSummary(ESC_RAW) ?></p>
 		<p><?php echo __('To read more information about this project click ').link_to(__('here'), 'project/show?id='.$project->getId()).'.'; ?></p>
 		<p><?php echo __('Website: ').link_to($project->getUnit()->getUrl(), 'http://'.$project->getUnit()->getUrl()); ?></p>
