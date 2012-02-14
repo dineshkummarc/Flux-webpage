3c91ae5e01d2976e9a9035954b4cd35ca1c1937e
David Romaní <david@flux.cat>
2012-02-14 12:30:33 +0100

    Corrector parse III

--- apps/frontend/modules/project/templates/showSuccess.php
+++ apps/frontend/modules/project/templates/showSuccess.php
@@ -1,3 +1,5 @@
+<?php use_helper('Date')?>
+
 <h1><?php echo __('projects')?></h1>
 
 <?php if (strlen($project->getLogo()) > 0): ?>
@@ -17,6 +19,10 @@
 
 <?php if (strlen($project->getUnit()->getUrl())): ?>
 	<p><strong><?php echo __('Homepage').': ' ?></strong><?php echo link_to($project->getUnit()->getUrl(), 'http://'.$project->getUnit()->getUrl()) ?></p>
+<?php endif; ?>
+
+<?php if (strlen($project->getBeginDate())): ?>
+	<p><strong><?php echo __('Date').': ' ?></strong><?php echo format_date($project->getBeginDate()) ?></p>
 <?php endif; ?>
 
 <?php if (strlen($project->getTechnology())): ?>
